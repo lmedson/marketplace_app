ceccaff7d704848bdc66129a4f9d3d53f2713511
lmedson <medson-mbp@hotmail.com>
2018-11-26 18:25:32 -0300

    Add paginators and filters

--- src/app/controllers/AdController.js
+++ src/app/controllers/AdController.js
@@ -3,7 +3,30 @@ const Ad = require('../models/Ad')
 class AdController {
   async index (req, res, next) {
     try {
-      const ads = await Ad.find({})
+      const filters = {}
+
+      if (req.query.price_min || req.query.price_max) {
+        filters.price = {}
+
+        if (req.query.price_min) {
+          filters.price.$gte = req.query.price_min
+        }
+
+        if (req.query.price_max) {
+          filters.price.$lte = req.query.price_max
+        }
+      }
+
+      if (req.query.title) {
+        filters.title = new RegExp(req.query.title, 'i')
+      }
+
+      const ads = await Ad.paginate(filters, {
+        page: req.query.page || 1,
+        limit: 20,
+        populate: ['author'],
+        sort: '-createdAt'
+      })
 
       return res.status(200).json(ads)
     } catch (err) {
